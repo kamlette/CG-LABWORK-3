3119f9312181a403a6260dad716d3d987ccc4632
kamlette <92592624+kamlette@users.noreply.github.com>
2022-05-28 14:30:17 +0500

    Добавьте файлы проекта.

---
+++ lighting_technique.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#ifndef LIGHTINGTECHNIQUE_H
+#define LIGHTINGTECHNIQUE_H
+
+#include "technique.h"
+#include "math_3d.h"
+
+//ñòðóêòóðà íàïðàâëåííîãî ñâåòà
+struct DirectionLight
+{
+    Vector3f Color;
+    float AmbientIntensity;
+    Vector3f Direction;
+    float DiffuseIntensity;
+};
+
+class LightingTechnique : public Technique
+{
+public:
+    LightingTechnique();
+    virtual bool Init();
+
+    void SetWVP(const Matrix4f& WVP);
+    void SetWorldMatrix(const Matrix4f& WVP);
+    void SetTextureUnit(unsigned int TextureUnit);
+    void SetDirectionalLight(const DirectionLight& Light);
+
+    void SetEyeWorldPos(const Vector3f& EyeWorldPos);
+    void SetMatSpecularIntensity(float Intensity);
+    void SetMatSpecularPower(float Power);
+
+private:
+    GLuint m_WVPLocation;
+    GLuint m_WorldMatrixLocation;
+    GLuint m_samplerLocation;
+
+    GLuint m_eyeWorldPosition;
+    GLuint m_matSpecularIntensityLocation;
+    GLuint m_matSpecularPowerLocation;
+
+    struct {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint Direction;
+        GLuint DiffuseIntensity;
+    } m_dirLightLocation;
+};
+
+#endif // LIGHTINGTECHNIQUE_H
